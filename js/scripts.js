bb5d0d65ad813bc4cadc42d6f22cd76fd12583db
Brian Nelson and Vera Weikel <brian@>
2018-08-22 16:43:39 -0700

    add x and o

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,76 @@
-var players = [];
-var board = ['0','1','2','3','4','5','6','7','8','9'];
-var game = ['1','0','0','0','0','0','0','0','0','0'];
 var playerTurnIndex = 0;
-var finalScore = 25;
+var players = [];
+var board = ['0','1','2','3','4','5','6','7','8',];
+var game = ['-','-','-','-','-','-','-','-','-',];
+var winningArrs = [
+  ['M','M','M','-','-','-','-','-','-',],
+  ['-','-','-','M','M','M','-','-','-',],
+  ['-','-','-','-','-','-','M','M','M',],
+  ['M','-','-','M','-','-','M','-','-',],
+  ['-','M','-','-','M','-','-','M','-',],
+  ['-','-','M','-','-','M','-','-','M',],
+  ['M','-','-','-','M','-','-','M','-',],
+  ['-','-','M','-','M','-','M','-','-',],
+];
 
-var Player = function (name, symbol) {
+var Player = function (name, symbol, turn) {
   this.name = name;
   this.symbol = symbol;
-  this.turn = false;
-  this.grid = [];
+  this.turn = turn;
+  this.grid = ['-','-','-','-','-','-','-','-','-','-'];
 }
 
 var checkWin = function () {
-  if ((game[1] === game[2] === game[3]) || (game[4] === game[5] === game[6]) || (game[7] === game[8] === game[9]) || (game[1] === game[5] === game[9]) || (game[3] === game[5] === game[7]) || (game[1] === game[4] === game[7]) || (game[3] === game[6] === game[9])) {
-    return true;
+  if (
+    false
+  ) {
+  return true;
   } else {
-  return false
+  return false;
   }
-
-game.includes(players.symbol)
 }
 
+var winningArrs = function () {
 
+  for(i=0;i<winningArrs.length-1;i++) {
+
+  }
+}
+
+var switchTurn = function() {
+  if (players[playerTurnIndex].turn = true) {
+    players[playerTurnIndex].turn = false;
+    if (playerTurnIndex === players.length-1) {
+      playerTurnIndex = 0;
+    } else {
+      playerTurnIndex += 1;
+    }
+    players[playerTurnIndex].turn = true;
+  } else {
+    players[playerTurnIndex].turn = true;
+  }
+}
 
 $(document).ready(function() {
   var player1 = $('#player1').val();
   var player2 = $('#player2').val();
 
-  var newPlayer = new Player("Joe", 'X');
+  var newPlayer = new Player("Player 1", 'X', true);
   players.push(newPlayer);
-  var newPlayer = new Player("Bob", 'O');
+  var newPlayer = new Player("Player 2", 'O', false);
   players.push(newPlayer);
-  $('#begin').click(function() {
-  });
 
+  $('.pTurn').text(players[playerTurnIndex].name + " - " + players[playerTurnIndex].symbol)
   $('.tSquare').click(function() {
-    $(this).text(players[0].symbol);
-
+    $(this).text(players[playerTurnIndex].symbol);
+    game[parseInt($(this).attr('id'))] = "M";
+    players[playerTurnIndex].grid[parseInt($(this).attr('id'))] = players[playerTurnIndex].symbol;
+    if (checkWin()) {
+      $('.winner').text(players[playerTurnIndex].name + " is the winner");
+    } else {
+      switchTurn();
+      $('.pTurn').text(players[playerTurnIndex].name + " - " + players[playerTurnIndex].symbol)
+    }
   });
 
 });
