d75d0d963f11be4813e4d7a466fb274b9989fa7f
Brian Nelson and Vera Weikel <vera@>
2018-08-23 15:34:58 -0700

    add comments to JS and clean up unused code

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,37 @@
+//initial global variables
 var game = [];
-var board = [];
 var playerTurnIndex = 0;
 var players = [];
+var gameOver = 0;
 
-var gameSetup = function() {
+
+var gameSetup = function() {//setup game with new players and a new gameboard, clear fields out
   players = [];
   var newPlayer = new Player("Player 1", 'X', true);
   players.push(newPlayer);
   var newPlayer = new Player("Player 2", 'O', false);
   players.push(newPlayer);
   $('.pTurn').text(players[playerTurnIndex].name + " - " + players[playerTurnIndex].symbol)
-  board = ['0','1','2','3','4','5','6','7','8'];
   game = ['Z','Z','Z','Z','Z','Z','Z','Z','Z'];
   playerTurnIndex = 0;
   // for(i=0;i<8;i++) {
     $('.tSquare').text('');
 }
 
-var winningArrs = [
-  ['M','M','M','-','-','-','-','-','-'],
-  ['-','-','-','M','M','M','-','-','-'],
-  ['-','-','-','-','-','-','M','M','M'],
-  ['M','-','-','M','-','-','M','-','-'],
-  ['-','M','-','-','M','-','-','M','-'],
-  ['-','-','M','-','-','M','-','-','M'],
-  ['M','-','-','-','M','-','-','-','M'],
-  ['-','-','M','-','M','-','M','-','-']
-];
+// var winningArrs = [
+//   ['M','M','M','-','-','-','-','-','-'],
+//   ['-','-','-','M','M','M','-','-','-'],
+//   ['-','-','-','-','-','-','M','M','M'],
+//   ['M','-','-','M','-','-','M','-','-'],
+//   ['-','M','-','-','M','-','-','M','-'],
+//   ['-','-','M','-','-','M','-','-','M'],
+//   ['M','-','-','-','M','-','-','-','M'],
+//   ['-','-','M','-','M','-','M','-','-']
+// ];
 
 
 
-var otherWin = [
+var winningArray = [
   ['0','1','2'],
   ['3','4','5'],
   ['6','7','8'],
@@ -44,28 +45,27 @@ var otherWin = [
 
 //Returns all the string of indexes of the players moves
 var findWin = function() {
-  if (playerTurnIndex === 0) {
+  if (playerTurnIndex === 0) { //set variable to search for X or O
     var turns = (players[playerTurnIndex].grid.join('').match(/x/gi)||[]).length;
   } else {
     var turns = (players[playerTurnIndex].grid.join('').match(/o/gi)||[]).length;
   }
   var index = 0;
   var output = '';
-  for(i = 0;i<turns;i++) {
+  for(i = 0;i<turns;i++) { //create a output string that is the indexes of the player.grid
     index = players[playerTurnIndex].grid.join('').indexOf(players[playerTurnIndex].symbol,index);
     output += index;
     index ++;
   }
   playerArr = output.split('');
-  for(i=0;i<otherWin.length;i++) {
-    if (playerArr.includes(otherWin[i][0]) && playerArr.includes(otherWin[i][1]) && playerArr.includes(otherWin[i][2])) {
-
+  for(i=0;i<winningArray.length;i++) { //see if the current player.grid is a one of the winning arrays
+    if (playerArr.includes(winningArray[i][0]) && playerArr.includes(winningArray[i][1]) && playerArr.includes(winningArray[i][2])) {
       return true;
     }
   }
 }
 
-var isTied = function() {
+var isTied = function() { // check to see if any Z's are left in the game array
   var spacesRemaining = game.join('').match(/z/gi)||[].length;
   if (spacesRemaining === 0) {
     return true;
@@ -73,24 +73,14 @@ var isTied = function() {
 }
 
 
-var Player = function (name, symbol, turn) {
+var Player = function (name, symbol, turn) {// player objects with name, symbol, isTurn, and grid
   this.name = name;
   this.symbol = symbol;
   this.turn = turn;
   this.grid = ['-','-','-','-','-','-','-','-','-'];
 }
 
-var checkWin = function () {
-  if (
-    false
-  ) {
-  return true;
-  } else {
-  return false;
-  }
-}
-
-var resetGame = function() {
+var resetGame = function() { //reset the game and call the initial setup to play again
   gameOver = 0
   players = [];
   gameSetup();
@@ -98,7 +88,7 @@ var resetGame = function() {
 }
 
 
-var switchTurn = function() {
+var switchTurn = function() {//sets first turn to player[0] OR alternates to next player in player[]
   if (players[playerTurnIndex].turn = true) {
     players[playerTurnIndex].turn = false;
     if (playerTurnIndex === players.length-1) {
@@ -113,35 +103,35 @@ var switchTurn = function() {
 }
 
 
-var gameOver = 0;
+
 $(document).ready(function() {
   gameSetup();
-  $('.tSquare').click(function() {
+  $('.tSquare').click(function() {//when user clicks on any of the 9 squares on the grid
     if (gameOver === 0) {
       if (game[parseInt($(this).attr('id'))] === "Z") {
         $(this).text(players[playerTurnIndex].symbol);
         game[parseInt($(this).attr('id'))] = players[playerTurnIndex].symbol;
         players[playerTurnIndex].grid[parseInt($(this).attr('id'))] = players[playerTurnIndex].symbol;
-        if (findWin()) {
+        if (findWin()) {//check to see if the last move was a winning move
           $('.winner').text(players[playerTurnIndex].name + " is the winner");
           $('.winner').show();
           gameOver = 1;
-        } else if (isTied()) {
+        } else if (isTied()) {//check to see if any empty spaces are left, otherwise TIE game
           gameOver = 1;
           $('.winner').text("There is no winner. Click the Reset button to play again.");
           $('.winner').show();
-        } else {
+        } else {//switch the turn to the next player
           switchTurn();
           $('.pTurn').text(players[playerTurnIndex].name + " - " + players[playerTurnIndex].symbol)
         }
-      } else {
+      } else {//user clicked on a square that the game board that is not empty (has a Z)
         alert("Choose a square that hasn't been taken");
       }
-    } else {
+    } else {//Game is over and clicking on grid will disable function
       alert("Game is over.");
     }
   });
-  $('#reset').click(function() {
+  $('#reset').click(function() {//reset game and will call the gameSetup()
     resetGame();
   });
 });
